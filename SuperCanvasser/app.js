e2f39f9e7deca1aac8776fa7d03ddcfa38954116
samleung314 <samleung314@gmail.com>
2018-11-28 16:39:59 -0500

    https working

--- SuperCanvasser/app.js
+++ SuperCanvasser/app.js
@@ -12,9 +12,6 @@ var cookieParser = require('cookie-parser');
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
-var privateKey  = fs.readFileSync('./key.key', 'utf8');
-var certificate = fs.readFileSync('./crt.crt', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
 
 var winston = require('./winston');
 var morgan = require('morgan');
@@ -55,10 +52,24 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+//start http and https server
+//http://35.237.133.218:8080
+//https://35.237.133.218/:8443
 var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
+var httpsServer = https.createServer({
+  key: fs.readFileSync('server-key.pem'),
+  cert: fs.readFileSync('server-cert.pem')
+}, app);
 httpServer.listen(8080);
 httpsServer.listen(8443);
+
 // start server
 //app.listen(80, () => console.log('SuperCanvasser listening on port 80!'))
 module.exports = app;
+
+/*
+npm install node-gyp -g
+npm install bcrypt -g
+
+npm install bcrypt -save
+*/
